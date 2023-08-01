596259399bab2e53fd12399d5d343b280b789f74
Ashley Plasket <ashley.plasket@gmail.com>
2023-08-01 09:18:57 -0400

    add test for index spec

---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe "/, root path" do
+  describe "as a user, when i visit root path" do
+    it "I select 'Griffin Coffee' form the start location drop down" do
+      visit root_path
+
+      expect(page).to have_content("Nearest Fuel Station")
+      expect(page).to have_content("Search For The Nearest Electric Charging Station")
+      expect(page).to have_button("Find Nearest Station")
+
+      click_on "Find Nearest Station"
+
+      expect(current_path).to eq(search_path)
+    end
+  end
+end
